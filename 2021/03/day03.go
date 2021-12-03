7d2715762bf4342bec9cfb371154b2ef1baf033c
Daniel <daniel@endlesslycurious.com>
2021-12-03 08:36:37 -0800

    Day 3 skeleton files

---
+++ 2021/03/day03.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func ReadPowerBits(filename string) []string {
+	data := make([]string, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		data = append(data, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Loaded ", len(data), " readings from ", filename)
+
+	return data
+}
+
+func main() {
+	input := ReadPowerBits("./2021/03/input.txt")
+
+	answer := Part1(input)
+	fmt.Println("Part 1 answer: ", answer)
+}
+
+func Part1(data []string) int {
+	return 0
+}
---
+++ 2021/03/day03_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+var input = []string{
+	"00100",
+	"11110",
+	"10110",
+	"10111",
+	"10101",
+	"01111",
+	"00111",
+	"11100",
+	"10000",
+	"11001",
+	"00010",
+	"01010",
+}
+
+func TestPart1(t *testing.T) {
+	result := 22
+	actual := Part1(input)
+	if actual != result {
+		log.Fatalln("FAIL! expected ", result, " got ", actual)
+	}
+}
+
+var benchInput = ReadPowerBits("./input.txt")
+
+func BenchmarkPart1(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Part1(benchInput)
+	}
+}
