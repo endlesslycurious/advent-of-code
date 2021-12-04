b70475ee8a5ed543d2c5028d168e1a146a6d45ac
Daniel <daniel@endlesslycurious.com>
2021-12-04 12:34:45 -0800

    Implemented bingo numbers and board loading from file

--- 2021/04/day04.go
+++ 2021/04/day04.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 const (
-	boardSide    = 5
-	boardNumbers = boardSide * boardSide
+	boardSide     = 5
+	boardNumCount = boardSide * boardSide
 )
 
 // Represents a 5x5 bingo board
 type Board struct {
-	Numbers [boardNumbers]int
+	Numbers []int
 	Score   int
 }
 
@@ -18,10 +25,76 @@ func (b *Board) Update(num int) int {
 	return 0
 }
 
+func ParseInt(in string) int {
+	num, err := strconv.Atoi(in)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return num
+}
+
 // read bingo numbers and boards from text file
 func ReadInputs(filename string) ([]int, []Board) {
-	numbers := []int{}
-	boards := []Board{}
+	numbers := make([]int, 0)
+	boards := make([]Board, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// first line is the bingo numbers
+	read := scanner.Scan()
+	if !read {
+		panic("Problem reading numbers line")
+	}
+
+	numLine := scanner.Text()
+	for _, numStr := range strings.Split(numLine, ",") {
+		num := ParseInt(numStr)
+		numbers = append(numbers, num)
+	}
+
+	// rest of file is board definitions
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		// new line marks start of a board definition, next X lines are the rows
+		if len(line) == 0 {
+			boardNumbers := make([]int, 0, boardNumCount)
+
+			for i := 0; i < boardSide; i++ {
+				read := scanner.Scan()
+				if !read {
+					panic("Problem reading board line")
+				}
+
+				rowStr := scanner.Text()
+				for _, numStr := range strings.Split(rowStr, " ") {
+					// space padded input numbers
+					if len(numStr) > 0 {
+						num := ParseInt(numStr)
+						boardNumbers = append(boardNumbers, num)
+					}
+				}
+			}
+
+			board := Board{boardNumbers, 0}
+			boards = append(boards, board)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Loaded ", len(numbers), " numbers and ", len(boards), " boards from ", filename)
 
 	return numbers, boards
 }
