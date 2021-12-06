df60bea49c749151e9b22491500f77676544cd27
Daniel <daniel@endlesslycurious.com>
2021-12-06 12:12:42 -0800

    Simplify a bit part1 line direction handling

--- 2021/05/day05.go
+++ 2021/05/day05.go
@@ -171,25 +171,25 @@ func Part1(input []Line, topLeft, bottomRight Point) int {
 	for _, line := range input {
 		if line.Vertical() {
 			// line direction may not be positive along y!
-			if line.start.y <= line.end.y {
-				for y := line.start.y; y <= line.end.y; y++ {
-					grid.Increment(line.start.x, y)
-				}
-			} else {
-				for y := line.end.y; y <= line.start.y; y++ {
-					grid.Increment(line.start.x, y)
-				}
+			begin, end := line.start.y, line.end.y
+
+			if begin > end {
+				begin, end = end, begin
+			}
+
+			for y := begin; y <= end; y++ {
+				grid.Increment(line.start.x, y)
 			}
 		} else if line.Horizontal() {
 			// line direction may not be positive along x!
-			if line.start.x <= line.end.x {
-				for x := line.start.x; x <= line.end.x; x++ {
-					grid.Increment(x, line.start.y)
-				}
-			} else {
-				for x := line.end.x; x <= line.start.x; x++ {
-					grid.Increment(x, line.start.y)
-				}
+			begin, end := line.start.x, line.end.x
+
+			if begin > end {
+				begin, end = end, begin
+			}
+
+			for x := begin; x <= end; x++ {
+				grid.Increment(x, line.start.y)
 			}
 		}
 	}
