6b6aa6c8703795c7f35e635fb6ea8ef1dde89a9b
Daniel <daniel@endlesslycurious.com>
2021-12-07 19:24:42 -0800

    Load fish from file, add slow test of part 1 too

--- 2021/06/day06.go
+++ 2021/06/day06.go
@@ -1,9 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadInput(filename string) []int {
+	data := make([]int, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	line := strings.TrimSpace(scanner.Text())
+	for _, str := range strings.Split(line, ",") {
+		fish, err := strconv.Atoi(str)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		data = append(data, fish)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Loaded ", len(data), " fish from ", filename)
+
+	return data
+}
 
 func main() {
-	var input []int
+	input := ReadInput("./2021/06/input.txt")
 
 	answer := Part1(input, 80)
 	fmt.Println("Answer Part 1:", answer)
--- 2021/06/day06_test.go
+++ 2021/06/day06_test.go
@@ -7,9 +7,19 @@ import (
 
 var input = []int{3, 4, 3, 1, 2}
 
-func TestPart1(T *testing.T) {
+func TestPart1Short(T *testing.T) {
 	expected := 26
-	days := 26
+	days := 18
+	actual := Part1(input, days)
+
+	if actual != expected {
+		log.Fatalln("FAIL! Expected", expected, "got", actual)
+	}
+}
+
+func TestPart1Long(T *testing.T) {
+	expected := 5934
+	days := 80
 	actual := Part1(input, days)
 
 	if actual != expected {
