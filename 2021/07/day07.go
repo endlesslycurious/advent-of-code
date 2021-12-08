42202dda1cd607fae8843232de445b6c80b0c388
Daniel <daniel@endlesslycurious.com>
2021-12-07 20:27:39 -0800

    File loading

--- 2021/07/day07.go
+++ 2021/07/day07.go
@@ -1,9 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func ReadInput(filename string) []int {
-	return []int{}
+	data := make([]int, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	line := strings.TrimSpace(scanner.Text())
+	for _, str := range strings.Split(line, ",") {
+		fish, err := strconv.Atoi(str)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		data = append(data, fish)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Loaded ", len(data), " crabs from ", filename)
+
+	return data
 }
 
 func main() {
