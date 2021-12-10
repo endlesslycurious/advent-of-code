18e7aa006ff238a7f7222888aa70582351016ce7
Daniel <daniel@endlesslycurious.com>
2021-12-10 09:10:36 -0800

    Day 9 part 1 solution, create grid struct with GetHeight and GetNeighboursHeights methods

--- 2021/09/day09.go
+++ 2021/09/day09.go
@@ -50,9 +50,85 @@ func main() {
 	fmt.Println("Loaded", len(input), "lines from", filename)
 
 	answer := Part1(input)
-	fmt.Println("Answer Part 1", answer)
+	fmt.Println("Answer Part 1:", answer)
 }
 
-func Part1([][]int) int {
-	return 0
+type Grid struct {
+	data   [][]int // points arranged [y][x], indexed from 0
+	width  int
+	height int
+}
+
+func CreateGrid(data [][]int) Grid {
+	width := len(data[0])
+	height := len(data)
+	return Grid{data, width, height}
+}
+
+// Get hieght at point x,y
+func (g Grid) GetHeight(x, y int) int {
+	if x < 0 || x >= g.width {
+		log.Fatalln("Get", x, y, "x out of range 0-", g.width)
+	} else if y < 0 || y >= g.height {
+		log.Fatalln("Get", x, y, "y out of range 0-", g.height)
+	}
+
+	return g.data[y][x]
+}
+
+// Get heights of neighbours of point x,y
+func (g Grid) GetNeighboursHeights(x, y int) []int {
+	if x < 0 || x >= g.width {
+		log.Fatalln("Get", x, y, "x out of range 0-", g.width)
+	} else if y < 0 || y >= g.height {
+		log.Fatalln("Get", x, y, "y out of range 0-", g.height)
+	}
+
+	heights := make([]int, 0, 4)
+
+	if x == 0 {
+		heights = append(heights, g.GetHeight(x+1, y))
+	} else if x == (g.width - 1) {
+		heights = append(heights, g.GetHeight(x-1, y))
+	} else {
+		heights = append(heights, g.GetHeight(x+1, y))
+		heights = append(heights, g.GetHeight(x-1, y))
+	}
+
+	if y == 0 {
+		heights = append(heights, g.GetHeight(x, y+1))
+	} else if y == (g.height - 1) {
+		heights = append(heights, g.GetHeight(x, y-1))
+	} else {
+		heights = append(heights, g.GetHeight(x, y+1))
+		heights = append(heights, g.GetHeight(x, y-1))
+	}
+
+	return heights
+}
+
+func Part1(input [][]int) int {
+	grid := CreateGrid(input)
+	var risk int
+
+	for x := 0; x < grid.width; x++ {
+		for y := 0; y < grid.height; y++ {
+			pointHeight := grid.GetHeight(x, y)
+			neighbours := grid.GetNeighboursHeights(x, y)
+
+			lowest := true
+			for _, height := range neighbours {
+				if height <= pointHeight {
+					lowest = false
+					break
+				}
+
+			}
+			if lowest {
+				risk += pointHeight + 1
+			}
+		}
+	}
+
+	return risk
 }
--- 2021/09/day09_test.go
+++ 2021/09/day09_test.go
@@ -10,6 +10,42 @@ var input = [][]int{
 	{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
 }
 
+func TestGetHeight(t *testing.T) {
+	grid := CreateGrid(input)
+	inputs := [][2]int{{0, 0}, {grid.width - 1, grid.height - 1}, {1, 4}, {3, 1}}
+	expected := []int{2, 8, 8, 7}
+
+	for i, point := range inputs {
+		actual := grid.GetHeight(point[0], point[1])
+
+		if actual != expected[i] {
+			t.Error("Get", point, "expected", expected, "got", actual)
+		}
+	}
+}
+
+func TestGetNeighboursHeights(t *testing.T) {
+	grid := CreateGrid(input)
+	inputs := [][2]int{{0, 0}, {grid.width - 1, grid.height - 1}, {1, 4}, {3, 1}, {9, 1}}
+	outputs := [][]int{{1, 3}, {7, 9}, {9, 9, 7}, {8, 8, 6, 9}, {2, 2, 0}}
+
+	for i, point := range inputs {
+		expected := outputs[i]
+		actual := grid.GetNeighboursHeights(point[0], point[1])
+
+		if len(actual) != len(expected) {
+			t.Error("Get", point, "expected length", len(expected), "got", len(actual))
+		}
+
+		for idx := range expected {
+			if actual[idx] != expected[idx] {
+				t.Error("Get", point, "expected", expected, "got", actual)
+				break
+			}
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	expected := 15
 
