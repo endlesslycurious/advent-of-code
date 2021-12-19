b4c9302e25f5619c65a4539b2498137a0da8e446
Daniel <daniel@endlesslycurious.com>
2021-12-19 13:58:38 -0800

    Day 10 initial files

---
+++ 2021/10/day10.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func ReadInput(filename string) []string {
+	return []string{}
+}
+
+func main() {
+	input := ReadInput("./2021/10/day10_input.txt")
+
+	answer := Part1(input)
+	fmt.Println("Part 1 Answer:", answer)
+}
+
+func Part1(input []string) int {
+	var score int
+	return score
+}
