7b5ab84c71db1168d5bfd6deb358eea7a59e74b5
Daniel <daniel@endlesslycurious.com>
2021-12-19 14:04:57 -0800

    File loading

--- 2021/10/day10.go
+++ 2021/10/day10.go
@@ -1,13 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func ReadInput(filename string) []string {
-	return []string{}
+	data := make([]string, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		data = append(data, line)
+	}
+
+	return data
 }
 
 func main() {
-	input := ReadInput("./2021/10/day10_input.txt")
+	filename := "./2021/10/day10_input.txt"
+	input := ReadInput(filename)
+	fmt.Println("Loaded", len(input), "lines from", filename)
 
 	answer := Part1(input)
 	fmt.Println("Part 1 Answer:", answer)
