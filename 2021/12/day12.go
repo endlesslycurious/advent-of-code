cdc0e0b6f5c502eacabec77a781d83b6f6390370
Daniel <daniel@endlesslycurious.com>
2021-12-20 17:07:53 -0800

    Input file reading and graph creation with tests

--- 2021/12/day12.go
+++ 2021/12/day12.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 // Represents a cave of Node in the graph
 type Node struct {
@@ -9,19 +15,75 @@ type Node struct {
 	connections []*Node // list of connected caves/nodes
 }
 
+// Create node graph from input lines
+func CreateNodeGraph(lines []string) []Node {
+	nodeMap := make(map[string]*Node)
+
+	for _, line := range lines {
+
+		caves := strings.Split(line, "-")
+		if len(caves) != 2 {
+			log.Fatalln("Expected two caves only, not", len(caves))
+		}
+
+		// create nodes in map and list
+		for _, cave := range caves {
+			_, exists := nodeMap[cave]
+
+			if !exists {
+				big := cave == strings.ToUpper(cave)
+				node := &Node{cave, big, make([]*Node, 0)}
+				nodeMap[cave] = node
+			}
+		}
+
+		// connect nodes
+		start := caves[0]
+		end := caves[1]
+
+		nodeMap[start].connections = append(nodeMap[start].connections, nodeMap[end])
+		nodeMap[end].connections = append(nodeMap[end].connections, nodeMap[start])
+	}
+
+	graph := make([]Node, 0, len(nodeMap))
+
+	for _, node := range nodeMap {
+		graph = append(graph, *node)
+	}
+
+	return graph
+}
+
 func ReadInput(filename string) []Node {
-	return []Node{}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		lines = append(lines, line)
+	}
+
+	graph := CreateNodeGraph(lines)
+
+	return graph
 }
 
 func main() {
 	filename := "./2021/12/day12_input.txt"
-	input := ReadInput(filename)
-	fmt.Println("Read", len(input), "nodes from", filename)
+	graph := ReadInput(filename)
+	fmt.Println("Read", len(graph), "nodes from", filename)
 
-	answer := Part1(input)
+	answer := Part1(graph)
 	fmt.Println("Part 1 Answer:", answer)
 }
 
-func Part1(nodes []Node) int {
+func Part1(graph []Node) int {
 	return 0
 }
--- 2021/12/day12_test.go
+++ 2021/12/day12_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 var input = []string{
 	"start-A",
@@ -12,9 +14,90 @@ var input = []string{
 	"b-end",
 }
 
+func TestCreateGraphSize(t *testing.T) {
+	graph := CreateNodeGraph(input)
+
+	actual := len(graph)
+	expected := 6
+
+	if actual != expected {
+		t.Error("Expected", expected, "got", actual)
+	}
+}
+
+func TestCreateGraphNames(t *testing.T) {
+	graph := CreateNodeGraph(input)
+	expected := []string{"start", "A", "b", "c", "d", "end"}
+
+	for _, name := range expected {
+		found := false
+		for _, node := range graph {
+			if node.name == name {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Error("Expected", name, "not found")
+			break
+		}
+	}
+}
+
+func TestCreateGraphLarge(t *testing.T) {
+	graph := CreateNodeGraph(input)
+	expected := map[string]bool{
+		"start": false,
+		"A":     true,
+		"b":     false,
+		"c":     false,
+		"d":     false,
+		"end":   false,
+	}
+
+	for _, node := range graph {
+		large, exists := expected[node.name]
+
+		if !exists {
+			t.Error("Unexpected", node.name, "node")
+		}
+
+		if node.large != large {
+			t.Error("Expected node", node.name, "large", large, "got", node.large)
+		}
+	}
+}
+
+func TestCreateGraphConnectionsCount(t *testing.T) {
+	graph := CreateNodeGraph(input)
+	expected := map[string]int{
+		"start": 2,
+		"A":     4,
+		"b":     4,
+		"c":     1,
+		"d":     1,
+		"end":   2,
+	}
+
+	for _, node := range graph {
+		count, exists := expected[node.name]
+
+		if !exists {
+			t.Error("Unexpected", node.name, "node")
+		}
+
+		actual := len(node.connections)
+		if actual != count {
+			t.Error("Expected node", node.name, "connection", count, "got", actual)
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	expected := 10
-	actual := Part1([]Node{})
+	graph := CreateNodeGraph(input)
+	actual := Part1(graph)
 
 	if actual != expected {
 		t.Error("Expected", expected, "got", actual)
