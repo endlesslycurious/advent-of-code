9a614e1074441930a2a44a666fee0c6d7140c8f7
Daniel <daniel@endlesslycurious.com>
2024-02-04 15:29:44 -0800

    Day zero (at last) initial file setup

---
+++ 2023/01/main.cpp
@@ -0,0 +1,39 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "inputs.h"
+
+// Print words to console and return word count
+auto process(const std::vector<std::string>& words) -> unsigned long
+{
+    unsigned long count = 0;
+    for (const std::string& word : words)
+    {
+        std::cout << word << " ";
+        count++;
+    }
+    std::cout << std::endl;
+
+    return count;
+}
+
+auto main() -> int
+{
+    std::cout << "-- Beginning testing! --" << std::endl;
+
+    // load inputs from text file
+    std::vector<std::string> inputs;
+    auto success = readInput(inputs);
+    assert(success);
+    assert(inputs.size() == read_input_test.size());
+
+    // process the inputs
+    auto count = process(inputs);
+    assert(count == inputs.size());
+
+    std::cout << "-- Testing passed! --" << std::endl;
+
+    return 0;
+}
