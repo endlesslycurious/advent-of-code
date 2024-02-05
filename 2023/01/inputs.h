c056755fc3e51ec068e81da66c457567e48cad0e
Daniel <daniel@endlesslycurious.com>
2024-02-04 16:11:28 -0800

    First working version of findCalibrationValue

--- 2023/01/inputs.h
+++ 2023/01/inputs.h
@@ -9,6 +9,14 @@
 // Input for function, would be read from file if was allot more data
 inline std::vector<std::string> read_input_test {"Hello", "C++", "Advent of Code", "2023!"};
 
+// Inputs to test findCalibrationValue NOLINTBEGIN
+inline std::vector<std::pair<unsigned int, std::string>> test_find_cal_vals= {
+    {12, "1abc2"},
+    {38, "pqr3stu8vwx"},
+    {15, "a1b2c3d4e5f"},
+    {77, "treb7uchet"}
+    }; // NOLINTEND
+
 // Read inputs from text file, use exception handling to prevent throws
 inline auto readInput(std::vector<std::string>& output) noexcept -> bool
 {
--- 2023/01/main.cpp
+++ 2023/01/main.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "inputs.h"
@@ -19,9 +20,43 @@ auto process(const std::vector<std::string>& words) -> unsigned long
     return count;
 }
 
+// Make double digit int using first and last digits found in source string
+auto findCalibrationValue(std::string& input) noexcept -> unsigned int{
+    int first{-1};
+    int last{-1};
+
+     for(char& chr :input)
+     {
+        if (chr >= '0' && chr <= '9')
+        {
+            int digit = atoi(&chr);
+            if( first == -1 && last == -1)
+            {
+                first = last = digit;
+            }else
+            {
+                last = digit;
+            }
+        }
+     }
+
+     std::string res_str = std::to_string(first) + std::to_string(last);
+     unsigned int res = stoi(res_str);
+
+    return res;
+}
+
 auto main() -> int
 {
     std::cout << "-- Beginning testing! --" << std::endl;
+
+    // test finding calibration values using supplied inputs & outputs
+    std::cout << " - Testing findCalibrationValue! -" << std::endl;
+    for( auto pair : test_find_cal_vals)
+    {
+        std::cout << "   " << pair.second << " => " << pair.first << std::endl;
+        assert(pair.first == findCalibrationValue(pair.second));
+    }
 
     // load inputs from text file
     std::vector<std::string> inputs;
