75a60ab163dd91d9b5bf94ec191a761896f2bbd7
Daniel <daniel@endlesslycurious.com>
2024-02-04 17:20:31 -0800

    Add try/catch to main with debug trap

--- 2023/00/main.cpp
+++ 2023/00/main.cpp
@@ -21,19 +21,29 @@ auto process(const std::vector<std::string>& words) -> unsigned long
 
 auto main() -> int
 {
-    std::cout << "-- Beginning testing! --" << std::endl;
+    try
+    {
+        std::cout << "-- Beginning testing! --" << std::endl;
 
-    // load inputs from text file
-    std::vector<std::string> inputs;
-    auto success = readInput(inputs);
-    assert(success);
-    assert(inputs.size() == read_input_test.size());
+        // load inputs from text file
+        std::vector<std::string> inputs;
+        auto success = readInput(inputs);
+        assert(success);
+        assert(inputs.size() == read_input_test.size());
 
-    // process the inputs
-    auto count = process(inputs);
-    assert(count == inputs.size());
+        // process the inputs
+        auto count = process(inputs);
+        assert(count == inputs.size());
 
-    std::cout << "-- Testing passed! --" << std::endl;
+        std::cout << "-- Testing passed! --" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        __builtin_debugtrap();
+        
+        return 1;
+    }
 
     return 0;
 }
