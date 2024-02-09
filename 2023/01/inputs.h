271e7233eda9fb3d0d98b94b675f2d611db08e9b
Daniel <daniel@endlesslycurious.com>
2024-02-08 20:31:41 -0800

    update comment

--- 2023/01/inputs.h
+++ 2023/01/inputs.h
@@ -9,7 +9,7 @@
 // Input for function, would be read from file if was allot more data
 inline std::vector<std::string> read_input_test {"Hello", "C++", "Advent of Code", "2023!"};
 
-// Inputs to test findCalibrationValue NOLINTBEGIN
+// Inputs to test findCalibrationValueP1 NOLINTBEGIN
 inline std::vector<std::pair<unsigned int, std::string>> test_find_cal_vals_p1= {
     {12, "1abc2"},
     {38, "pqr3stu8vwx"},
