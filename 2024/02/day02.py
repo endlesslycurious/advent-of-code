a5fd3df31ab320208961ed922c0530eb05696a71
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2025-01-04 16:50:02 -0800

    safety analysis implemented

--- 2024/02/day02.py
+++ 2024/02/day02.py
@@ -1,6 +1,38 @@
 """Advent of Code 2024 - Day 2 solution"""
 
 
+def analyse_safety(report: list[int]) -> bool:
+    """Analyse the safety of a report, true for safe"""
+    increase: int = 0
+    decrease: int = 0
+
+    for index, reading in enumerate(report):
+        if index == 0:
+            continue
+
+        previous: int = report[index - 1]
+
+        # Constant readings are unsafe
+        if reading == previous:
+            return False
+
+        # Large swings in readings are unsafe
+        if abs(reading - previous) > 3:
+            return False
+
+        if reading > previous:
+            increase += 1
+        else:
+            decrease += 1
+
+    # if not all increasing or decreasing then unsafe!
+    intervals: int = len(report) - 1
+    if (increase != intervals) & (decrease != intervals):
+        return False
+
+    return True
+
+
 def main() -> None:
     """Main function"""
     print("hello")
--- 2024/02/test_day02.py
+++ 2024/02/test_day02.py
@@ -2,5 +2,26 @@
 
 import unittest
 
+from day02 import analyse_safety
+
+
+class DayTwoTests(unittest.TestCase):
+    """Day two tests"""
+
+    def test_analyse_safety(self) -> None:
+        """Verify the analyse_safety method"""
+        inputs: list[tuple[list[int], bool]] = [
+            ([7, 6, 4, 2, 1], True),
+            ([1, 2, 7, 8, 9], False),
+            ([9, 7, 6, 2, 1], False),
+            ([1, 3, 2, 4, 5], False),
+            ([8, 6, 4, 4, 1], False),
+            ([1, 3, 6, 7, 9], True),
+        ]
+        for report, expected in inputs:
+            res: bool = analyse_safety(report)
+            self.assertEqual(res, expected, msg=f"{report} -> {res}, expected {expected}!")
+
+
 if __name__ == "__main__":
     unittest.main()
