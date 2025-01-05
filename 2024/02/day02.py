01f8eca978231bd4593f656aa8e8983feb74ff91
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2025-01-04 16:18:08 -0800

    Day 2 initial files

---
+++ 2024/02/day02.py
@@ -0,0 +1,10 @@
+"""Advent of Code 2024 - Day 2 solution"""
+
+
+def main() -> None:
+    """Main function"""
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/02/test_day02.py
@@ -0,0 +1,6 @@
+"""AoC 2024 - Day 2 tests"""
+
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
