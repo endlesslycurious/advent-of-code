744dde30246ef7acf9233133688ea940c95f3912
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2025-01-06 21:50:18 -0800

    Implement read_inputs with test

--- 2024/03/day03.py
+++ 2024/03/day03.py
@@ -1,8 +1,21 @@
 """Advent of Code 2024 - Day 3 solution"""
 
 
+def read_inputs(filename: str) -> list[str]:
+    """Read inputs from file"""
+    data: list[str] = []
+
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            data.append(line)
+
+    return data
+
+
 def part_one(filename: str) -> int:
     """Solve part one"""
+    data: list[str] = read_inputs(filename)
+
     return 0
 
 
--- 2024/03/test_day03.py
+++ 2024/03/test_day03.py
@@ -3,11 +3,19 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from day03 import part_one
+from day03 import part_one, read_inputs
 
 
 class Day03Tests(unittest.TestCase):
     """Day 3 tests"""
+
+    def test_read_inputs(self) -> None:
+        """Verify read_inputs works"""
+        data: str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+        with patch("builtins.open", mock_open(read_data=data)):
+            res: list[str] = read_inputs("test")
+            self.assertEqual(res, [data])
 
     def test_part_one(self) -> None:
         """Verify part one solution"""
